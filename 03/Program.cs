d006f37749aa1f23bbfee77a44e1f8c104e7606e
俊华 蔡 <1032031912@qq.com>
2020-09-15 10:01:32 +0800

    添加了访问类的修饰符使用：public/internal

---
+++ 03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    class Bird //默认的访问修饰符是internal (本程序集可以访问)
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("吱吱");
+        }
+    }
+    public class Dog//访问修饰符是public(任意可以访问)
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("旺旺");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird bird = new Bird();
+            Dog dog = new Dog();
+
+            bird.SayHello();//引用internal成员
+            dog.SayHello();//引用Public成员
+
+            Console.ReadKey();
+        }
+    }
+}
