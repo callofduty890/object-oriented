14579f6cd10f42af7e53313b4f27ca8e6167955e
俊华 蔡 <1032031912@qq.com>
2020-09-15 09:45:49 +0800

    1.添加了静态方法和实例方法 2.添加字段与两个构造函数

---
+++ 02_类的字段/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_类的字段
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(); //类的实例化形成对象
+            person1.Print(); //打印
+            Console.WriteLine("====================");
+            Person person2 = new Person("教练",18); //实例化形成对象
+            person2.Print();//打印
+            Console.ReadKey();
+        }
+    }
+
+    //类
+    public class Person
+    {
+        //字段
+        public string name;
+        protected int age;
+
+        //默认:构造函数
+        public Person()//不带参数的构造函数
+        {
+            this.name = "未知";
+            this.age = 0;
+        }
+        public Person(string name,int age)//带两个参数的构造函数
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void Print()   //输出人员信息
+        {
+            Console.WriteLine("name={0},age={1}", this.name, this.age);
+        }
+
+    }
+
+}
--- 9_15_object-oriented/Program.cs
+++ 9_15_object-oriented/Program.cs
@@ -8,8 +8,30 @@ namespace _9_15_object_oriented
 {
     class Program
     {
+        class MyHelloWorld
+        {
+            public static void SayHello()
+            {
+                Console.WriteLine("Hello World ! ");
+            }
+
+            public void CSayHello()
+            {
+                Console.WriteLine("你好");
+            }
+        }
+
         static void Main(string[] args)
         {
+            //静态的方法，无需实例化。在编译已经分配空间
+            MyHelloWorld.SayHello();
+            //非静态的方法,需要将类实例化形成对象。然后调用
+            MyHelloWorld myHelloWorld = new MyHelloWorld();
+            myHelloWorld.CSayHello();
+            //释放对象的内存空间
+            myHelloWorld = null;
+
+            Console.ReadKey();
         }
     }
 }
